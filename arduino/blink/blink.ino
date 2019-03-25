154f8554e5697cc34f2ee0779d76d992d096e978
Kaan Tasbas <mktasbas@gmail.com>
2019-03-25 11:08:11 -0400

    Test publisher on Arduino

--- arduino/blink/blink.ino
+++ arduino/blink/blink.ino
@@ -8,21 +8,39 @@
 
 ros::NodeHandle  nh;
 
-void messageCb( const argus_rail::RailStatus& rail_status){
-  if(rail_status.moving == true) digitalWrite(13, HIGH);
-  else if(rail_status.moving == false) digitalWrite(13, LOW);
-}
+argus_rail::RailStatus rail_status;
+ros::Publisher rail("/rail_status", &rail_status);
 
-ros::Subscriber<argus_rail::RailStatus> sub("/rail_status", &messageCb );
+// RailStatus message variables
+// header: uint32 seq, time stamp, string frame_id
+char frame_id[] = "/rail_status";
+uint8_t current_baseline = 3;
+bool moving = false;
+
+//void messageCb( const argus_rail::RailStatus& rail_status){
+//  if(rail_status.moving == true) digitalWrite(13, HIGH);
+//  else if(rail_status.moving == false) digitalWrite(13, LOW);
+//}
+//
+//ros::Subscriber<argus_rail::RailStatus> sub("/rail_status", &messageCb );
 
 void setup()
 { 
   pinMode(13, OUTPUT);
   nh.initNode();
-  nh.subscribe(sub);
+  nh.advertise(rail);
 }
 
 void loop()
 {  
+  current_baseline++;
+  if(current_baseline > 3) current_baseline = 1;
+  moving = !moving;
+  rail_status.baseline = current_baseline;
+  rail_status.moving = moving;
+  rail_status.header.frame_id = frame_id;
+  rail_status.header.stamp = nh.now();
+  rail.publish(&rail_status);
   nh.spinOnce();
+  delay(100);
 }
