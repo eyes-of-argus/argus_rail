23b05a1f919a794e4aebf81bcf2f240e9fae3cbe
Kaan Tasbas <mktasbas@gmail.com>
2019-03-26 18:27:13 -0400

    Test baseline request {user input} number of times

--- scripts/arduino_command.py
+++ scripts/arduino_command.py
@@ -6,15 +6,22 @@ from std_msgs.msg import UInt8
 def send():
     pub = rospy.Publisher('/rail_command', UInt8, queue_size=1)
     rospy.init_node('rail', anonymous=False)
-    rate = rospy.Rate(20)  # 1Hz
+    rate = rospy.Rate(1)  # 1Hz
     rail_command = UInt8()
     rail_command.data = 3
+    baseline = 3
+    run_count = 0
     while not rospy.is_shutdown():
-        rail_command.data = input()
-        if rail_command.data > 3:
-            rail_command.data = 3
-        elif rail_command.data < 1:
-            rail_command.data = 1
+        if(run_count > 0):
+            baseline = rail_command.data
+            baseline -= 1
+            if(baseline < 1):
+                baseline = 3
+            rail_command.data = baseline
+            run_count -= 1
+        else:
+            run_count = input()
+
         rospy.loginfo(rail_command)
         pub.publish(rail_command)
         rate.sleep()
